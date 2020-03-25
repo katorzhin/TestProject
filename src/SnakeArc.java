a5d8424790c068b0d20b0827ffba0c6c66c237e2
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-03-25 23:42:51 +0200

    Initial commit

---
+++ src/SnakeArc.java
@@ -0,0 +1,11 @@
+public class SnakeArc {
+    public static void main(String[] args) {
+        int array[][]=new int[1][1];
+        int[] arraySecond =new int[1];
+        snake(array);
+        snake(new int[][]{arraySecond});
+    }
+    public static Object snake(Object[] array){
+return array;
+    }
+}
